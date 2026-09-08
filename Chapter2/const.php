c0fd28448d9ed63cb09a293645ab8405cbb0da26
devidsmith9527-crypto <soeurnglim@gmail.com>
2026-09-08 20:42:23 +0700

    Create const.php

---
+++ Chapter2/const.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>const</title>
+</head>
+<body>
+    <?php
+        //const VARIABLE_NAME = value;
+        const PI = 3.14159;
+        const MAX_SIZE = 25;
+        echo "The value of PI is: " . PI . "<br>";
+        echo "The maximum number of students allowed is: " . MAX_SIZE . "<br>";
+    ?>
+</body>
+</html>
