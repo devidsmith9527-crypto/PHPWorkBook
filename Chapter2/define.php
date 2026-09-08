1999fc1089bcc391a2771bd03c80afb0b7b8070f
devidsmith9527-crypto <soeurnglim@gmail.com>
2026-09-08 20:43:36 +0700

    Create define.php

---
+++ Chapter2/define.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>define</title>
+</head>
+<body>
+    <?php
+        //define(VARIABLE_NAME, value);
+        define("PI", 3.14159);
+        define("MAX_SIZE", 25);
+        echo "The value of PI is: " . PI . "<br>";
+        echo "The maximum number of students allowed is: " . MAX_SIZE . "<br>";
+    ?>
+</body>
+</html>
